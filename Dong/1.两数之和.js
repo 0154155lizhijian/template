7cebde3699caa45c04aee283d4abc6877559b9c0
0154155lizhijian <1367688918@qq.com>
2021-11-30 22:40:47 +0800

    feat: twoSum的二分法解法

--- Dong/1.两数之和.js
+++ Dong/1.两数之和.js
@@ -11,17 +11,20 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-  // 这里就不用hashMap,用二分法求解
-  nums.sort((x, y) => x-y );
+  // 这里就不用hashMap了,用二分法求解
+  const numsIdnex = new Array(nums.length).fill(0);
+  for(i = 0; i< nums.length; i++){
+      numsIdnex[i] = i;
+  }
+  numsIdnex.sort((x, y) => nums[x] - nums[y] );
   for(let j=0; j < nums.length; j++){
-      const rightTarget = target - nums[j];
+      const rightTarget = target - nums[numsIdnex[j]];
       let left = j+1, right = nums.length;
       while(left < right){
           let mid = ((left + right) /2) >> 0;
-          if(nums[mid] === rightTarget){
-              console.log(j, mid);
-              return [j, mid];
-          }else if(nums[mid] > rightTarget){
+          if(nums[numsIdnex[mid]] === rightTarget){
+              return [numsIdnex[j],numsIdnex[mid]];
+          }else if(nums[numsIdnex[mid]] > rightTarget){
               right = mid;
           }else{
               left = mid +1;
