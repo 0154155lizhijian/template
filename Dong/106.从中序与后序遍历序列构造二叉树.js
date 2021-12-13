a53367a30effb71bab1eb020e5ec1b02dfc9e0a5
25055李智坚 <1367688918@qq.com>
2021-12-13 09:57:15 +0800

    feat: 后序中序找二叉树

--- Dong/106.从中序与后序遍历序列构造二叉树.js
+++ Dong/106.从中序与后序遍历序列构造二叉树.js
@@ -18,8 +18,29 @@
  * @param {number[]} postorder
  * @return {TreeNode}
  */
-var buildTree = function(inorder, postorder) {
-
+ var buildTree = function(inorder, postorder) {
+  return build(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
 };
+
+function build(inorder, iStart, iEnd, postorder, pStart, pEnd){
+  if(iStart > iEnd) return null;
+  // 找根节点
+  const rootVal = postorder[pEnd];
+  // 根节点在中序的位置
+  let index = 0;
+  for(let i = iStart; i<= iEnd; i++){
+      if(inorder[i] === rootVal){
+          index = i;
+          break;
+      }
+  }
+  // 计算左侧size
+  const leftSize = index-iStart;
+  // 遍历节点
+  const root = new TreeNode(rootVal);
+  root.left = build(inorder, iStart, index-1, postorder, pStart, pStart+leftSize-1);
+  root.right = build(inorder, index+1, iEnd, postorder, pStart+leftSize, pEnd-1);
+  return root; 
+}
 // @lc code=end
 
