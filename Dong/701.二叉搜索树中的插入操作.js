131d73b7b92efc5737481d24d127c486b956dcfe
25055李智坚 <1367688918@qq.com>
2021-12-14 15:09:29 +0800

    feat: 二叉搜索树插入

--- Dong/701.二叉搜索树中的插入操作.js
+++ Dong/701.二叉搜索树中的插入操作.js
@@ -19,7 +19,13 @@
  * @return {TreeNode}
  */
 var insertIntoBST = function(root, val) {
-
+  if(root === null) return new TreeNode(val);
+  if(root.val > val){
+    root.left = insertIntoBST(root.left, val);
+  }else{
+    root.right = insertIntoBST(root.right, val);
+  }
+  return root;
 };
 // @lc code=end
 
