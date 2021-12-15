08fd50e07cb36b92ea3512aafdf87e1551594ed1
25055李智坚 <1367688918@qq.com>
2021-12-15 15:08:42 +0800

    BST穷举

--- Dong/95.不同的二叉搜索树-ii.js
+++ Dong/95.不同的二叉搜索树-ii.js
@@ -18,27 +18,32 @@
  * @return {TreeNode[]}
  */
 var generateTrees = function(n) {
-  const build = (lo, hi) => {
+   /* 构造闭区间 [lo, hi] 组成的 BST */
+   const build = (lo, hi) => {
     let res = [];
+    // base case，显然当lo > hi闭区间[lo, hi]肯定是个空区间，也就对应着空节点 null，
     if(lo > hi){
-      res.push(null);
-      return res;
+        res.push(null); 
+        return res;
     }
-    for(let i =lo; i<= hi; i++){
-      let leftTree = build(lo, i-1);
-      let rightTree = build(i+1, hi);
-      for (const left of leftTree) {
-        for (const right of rightTree) {
-          const root = new TreeNode(i);
-          root.left = left;
-          root.right = right;
-          res.push(root);
+    // 穷举 root 节点的所有可能。
+    for(let i=lo; i<=hi; i++){
+        // 递归构造出左右子树的所有合法 BST
+        let leftTree = build(lo, i-1);
+        let rightTree = build(i+1, hi);
+        // 3、给 root 节点穷举所有左右子树的组合。
+        for(let left of leftTree){
+            for(let right of rightTree){
+                const root = new TreeNode(i);
+                root.left = left;
+                root.right = right;
+                res.push(root);
+            }
         }
-      }
     }
-    return res;
-  }
-  return build(1,n);
+   return res;
+}
+return build(1, n);
 };
 // @lc code=end
 
