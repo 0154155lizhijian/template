2eeb4e847d3567fc41c094181511f7d72fdc693b
25055李智坚 <1367688918@qq.com>
2021-12-15 15:08:53 +0800

    BST穷举

--- Dong/95.不同的二叉搜索树-ii.js
+++ Dong/95.不同的二叉搜索树-ii.js
@@ -17,33 +17,32 @@
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n) {
-   /* 构造闭区间 [lo, hi] 组成的 BST */
-   const build = (lo, hi) => {
+var generateTrees = function (n) {
+  /* 构造闭区间 [lo, hi] 组成的 BST */
+  const build = (lo, hi) => {
     let res = [];
     // base case，显然当lo > hi闭区间[lo, hi]肯定是个空区间，也就对应着空节点 null，
-    if(lo > hi){
-        res.push(null); 
-        return res;
+    if (lo > hi) {
+      res.push(null);
+      return res;
     }
     // 穷举 root 节点的所有可能。
-    for(let i=lo; i<=hi; i++){
-        // 递归构造出左右子树的所有合法 BST
-        let leftTree = build(lo, i-1);
-        let rightTree = build(i+1, hi);
-        // 3、给 root 节点穷举所有左右子树的组合。
-        for(let left of leftTree){
-            for(let right of rightTree){
-                const root = new TreeNode(i);
-                root.left = left;
-                root.right = right;
-                res.push(root);
-            }
+    for (let i = lo; i <= hi; i++) {
+      // 递归构造出左右子树的所有合法 BST
+      let leftTree = build(lo, i - 1);
+      let rightTree = build(i + 1, hi);
+      // 3、给 root 节点穷举所有左右子树的组合。
+      for (let left of leftTree) {
+        for (let right of rightTree) {
+          const root = new TreeNode(i);
+          root.left = left;
+          root.right = right;
+          res.push(root);
         }
+      }
     }
-   return res;
-}
-return build(1, n);
+    return res;
+  };
+  return build(1, n);
 };
 // @lc code=end
-
