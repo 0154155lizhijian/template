2378ef77fda616502f6b15777547abacd9f08bc6
25055李智坚 <1367688918@qq.com>
2021-12-21 16:53:46 +0800

    feat: 层级遍历,待完成

--- Dong/297.二叉树的序列化与反序列化.js
+++ Dong/297.二叉树的序列化与反序列化.js
@@ -62,53 +62,64 @@
 
 
  // 后序遍历
- const END = '#';
-/**
- * Encodes a tree to a single string.
- *
- * @param {TreeNode} root
- * @return {string}
- */
-var serialize = function(root) {
-    const result = traverse(root, []);
-    return result.join()
-};
+//  const END = '#';
+// /**
+//  * Encodes a tree to a single string.
+//  *
+//  * @param {TreeNode} root
+//  * @return {string}
+//  */
+// var serialize = function(root) {
+//     const result = traverse(root, []);
+//     return result.join()
+// };
 
-function traverse(root, res){
-    if(root == null){
-        res.push(END);
-        return res;
-    };
-    traverse(root.left, res);
-    traverse(root.right, res);
-    res.push(root.val);
-    return res;
-}
+// function traverse(root, res){
+//     if(root == null){
+//         res.push(END);
+//         return res;
+//     };
+//     traverse(root.left, res);
+//     traverse(root.right, res);
+//     res.push(root.val);
+//     return res;
+// }
 
-/**
- * Decodes your encoded data to tree.
- *
- * @param {string} data
- * @return {TreeNode}
- */
-var deserialize = function(data) {
-    const list = data.split(',');
-    return retree(list);
-};
+// /**
+//  * Decodes your encoded data to tree.
+//  *
+//  * @param {string} data
+//  * @return {TreeNode}
+//  */
+// var deserialize = function(data) {
+//     const list = data.split(',');
+//     return retree(list);
+// };
 
-function retree(data){
-    if(!data.length) return null;
-    if(data[data.length-1] === END){
-        data.pop();
-        return null;
-    }
-    const root = new TreeNode(data[data.length-1]);
-    data.pop();
-    root.right = retree(data);
-    root.left = retree(data);
-    return root;
-};
+// function retree(data){
+//     if(!data.length) return null;
+//     if(data[data.length-1] === END){
+//         data.pop();
+//         return null;
+//     }
+//     const root = new TreeNode(data[data.length-1]);
+//     data.pop();
+//     root.right = retree(data);
+//     root.left = retree(data);
+//     return root;
+// };
  
+// 层级遍历
+ var serialize = function(root) {
+    
+ };
+
+
+ var deserialize = function(data) {
+    
+};
+
+
  /**
   * Your functions will be called as such:
   * deserialize(serialize(root));
